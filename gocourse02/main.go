e0dcd90edffc1b858625e91bc23e1853c89c126d
Serhii Sokolov <serhii_sokolov@external.mckinsey.com>
2024-08-11 17:45:50 +0200

    feat: Homework2 - Removed majority of the comments - Renamed one field in Animal struct (inTheCage -> isCaged) - Renamed Cage's method (DescribeCage -> Describe) -

--- gocourse02/main.go
+++ gocourse02/main.go
@@ -2,94 +2,83 @@ package main
 
 import "fmt"
 
-// Struct that represents an Animal
 type Animal struct {
-	name      string // Name of the animal
-	species   string // Species of the animal
-	inTheCage bool   // Flag indicating whether the animal is in a cage
+	name    string
+	species string
+	isCaged bool
 }
 
-// Struct that represents a Zoo, containing multiple animals
 type Zoo struct {
-	Animals []Animal // Slice of animals in the zoo
+	Animals []Animal
 }
 
-// Struct that represents a Cage, which can hold an animal
 type Cage struct {
-	name   string // Name of the cage
-	animal string // Species of the animal currently in the cage
+	name   string
+	animal string
 }
 
-// Struct that represents all the cages in the zoo
 type AllCages struct {
-	Cages []Cage // Slice of cages
+	Cages []Cage
 }
 
-// Struct that represents a Zookeeper
 type Zookeeper struct {
-	name string // Name of the zookeeper
+	name string
 }
 
-// Constructor for creating a new Animal
 func NewAnimal(species string, name string) *Animal {
 	return &Animal{species: species, name: name}
 }
 
-// Constructor for creating a new Cage
 func NewCage(name string) *Cage {
 	return &Cage{name: name}
 }
 
-// Constructor for creating a new Zookeeper
 func NewZookeeper() *Zookeeper {
 	return &Zookeeper{name: "Bob"}
 }
 
-// Method that describes the current state of the cage
-func (c Cage) DescribeCage() {
+func (c Cage) Describe() {
 	if c.animal == "" {
-		fmt.Println("Cage is empty!\n") // Print if the cage is empty
+		fmt.Println("Cage is empty!")
 	} else {
-		fmt.Printf("Cage %s is occupied by animal: %s\n", c.name, c.animal) // Print the animal in the cage
+		fmt.Printf("Cage %s is occupied by animal: %s\n", c.name, c.animal)
 	}
 }
 
-// Function to create a new animal as a result of reproduction
 func ReproduceAnimals(species string, name string) *Animal {
-	return NewAnimal(species, name) // Calls the NewAnimal constructor to create the new animal
+	return NewAnimal(species, name)
 }
 
-// Method for adding animals to cages by the zookeeper
+// AddAnimalToCage садить всіх звірів із зоопарку в клітки.
+// Повертає nil, якщо тварина поміщена в клітку.
+// Повертає помилку, якщо всі клітки зайняті і тварина не поміщена в клітку.
 func (zk Zookeeper) AddAnimalToCage(c *AllCages, z *Zoo) error {
-	// Iterate over the animals in the zoo
+
 	for i := range z.Animals {
-		animal := &z.Animals[i] // Get a pointer to the current animal
-		if !animal.inTheCage {  // If the animal is not already in a cage
-			// Iterate over the cages
+		animal := &z.Animals[i]
+		if !animal.isCaged {
+
 			for j := range c.Cages {
-				cage := &c.Cages[j]    // Get a pointer to the current cage
-				if cage.animal == "" { // If the cage is empty
-					cage.animal = animal.species // Place the animal in the cage
-					animal.inTheCage = true      // Mark the animal as being in a cage
+				cage := &c.Cages[j]
+				if cage.animal == "" {
+					cage.animal = animal.species
+					animal.isCaged = true
 					fmt.Printf("Added %s to the cage %s\n", animal.name, cage.name)
-					break // Exit the cage loop as the animal has been placed
+					break
 				}
 			}
-			if !animal.inTheCage { // If animal is not in the cage, this means all cages are busy
-				return fmt.Errorf("All cages are occupied, %s is homeless", animal.name) // Return an error
+			if !animal.isCaged {
+				return fmt.Errorf("All cages are occupied, %s is homeless", animal.name)
 			}
 		}
 	}
-	return nil // Return nil if all animals were successfully placed in cages
+	return nil
 }
-
 func main() {
-	// Create new animals
+
 	lion := NewAnimal("lion", "Simba")
 	warthog := NewAnimal("warthog", "Pumba")
 	meerkat := NewAnimal("meerkat", "Timon")
-
-	// Initialize the zoo with the created animals
 	zoopark := Zoo{
 		Animals: []Animal{
 			*lion,
@@ -97,8 +86,6 @@ func main() {
 			*meerkat,
 		},
 	}
-
-	// Create cages for the animals
 	cage1 := NewCage("cage1")
 	cage2 := NewCage("cage2")
 	cage3 := NewCage("cage3")
@@ -109,28 +96,19 @@ func main() {
 			*cage3,
 		},
 	}
-
-	// Create a zookeeper
 	zk := NewZookeeper()
-
-	// Try to add animals to cages
 	err := zk.AddAnimalToCage(&cages, &zoopark)
+	// Якщо усі клітки зайняті то це ок, і доглядач зоопарку має сам вирішити це питання
 	if err != nil {
-		fmt.Println(err) // Print any error if animals couldn't be placed in cages
+		fmt.Println(err)
 	}
-
-	// Reproduce a new animal and add it to the zoo
 	newLion := ReproduceAnimals("lion", "Kiara")
-	zoopark.Animals = append(zoopark.Animals, *newLion) // Add the new animal to the zoo
-
-	// Try to add the new animal to a cage
+	zoopark.Animals = append(zoopark.Animals, *newLion)
 	err = zk.AddAnimalToCage(&cages, &zoopark)
 	if err != nil {
-		fmt.Println(err) // Print any error if the new animal couldn't be placed in a cage
+		fmt.Println(err)
 	}
-
-	// Describe the state of each cage
 	for _, c := range cages.Cages {
-		c.DescribeCage() // Print the details of each cage
+		c.Describe()
 	}
 }
