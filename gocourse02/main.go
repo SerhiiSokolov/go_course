b0a464ed46ea4c3f36382d78666ce1df450a4eca
Serhii Sokolov <serhii_sokolov@external.mckinsey.com>
2024-08-15 11:56:52 +0200

    feat: Homework2 - Added const for AnimalSpecies - Cage is a part of zoo -

--- gocourse02/main.go
+++ gocourse02/main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+const (
+	Lion    AnimalSpecies = "lion"
+	Warthog AnimalSpecies = "warthog"
+	Meerkat AnimalSpecies = "meerkat"
+)
+
+type AnimalSpecies string
+
 type Animal struct {
 	name    string
-	species string
+	species AnimalSpecies
 	cage    *Cage
 }
 
 type Zoo struct {
 	Animals []*Animal
+	Cages   []*Cage
 }
 
 type Cage struct {
@@ -17,24 +26,22 @@ type Cage struct {
 	animal *Animal
 }
 
-type AllCages struct {
-	Cages []Cage
-}
-
 type Zookeeper struct {
 	name string
 }
 
-func NewAnimal(species string, name string) *Animal {
+func NewAnimal(species AnimalSpecies, name string) *Animal {
 	return &Animal{species: species, name: name}
 }
+
+func NewCage(name string) *Cage {
+	return &Cage{name: name}
+}
+
 func (a *Animal) Escape() {
 	fmt.Printf("Animal %s escaped from the cage %v\n", a.name, a.cage.name)
 	a.cage.animal = nil
 	a.cage = nil
-}
-func NewCage(name string) *Cage {
-	return &Cage{name: name}
 }
 
 func NewZookeeper() *Zookeeper {
@@ -49,19 +56,19 @@ func (c Cage) Describe() {
 	}
 }
 
-func (a Animal) Reproduce(species string, name string) *Animal {
+func (a Animal) Reproduce(species AnimalSpecies, name string) *Animal {
 	return NewAnimal(species, name)
 }
 
-// AddAnimalToCage садить всіх звірів із зоопарку в клітки.
-// Повертає nil, якщо тварина поміщена в клітку.
-// Повертає помилку, якщо всі клітки зайняті і тварина не поміщена в клітку.
-func (zk Zookeeper) AddAnimalToCage(c *AllCages, z *Zoo) error {
+// AddAnimalToCage puts all animals to cages.
+// Function returns nil if all good.
+// Error is returned in case all cages are already occupied
+func (zk Zookeeper) AddAnimalToCage(z *Zoo) error {
 
 	for _, animal := range z.Animals {
 		if animal.cage == nil {
-			for j := range c.Cages {
-				cage := &c.Cages[j]
+			for j := range z.Cages {
+				cage := z.Cages[j]
 				if cage.animal == nil {
 					cage.animal = animal
 					animal.cage = cage
@@ -78,10 +85,16 @@ func (zk Zookeeper) AddAnimalToCage(c *AllCages, z *Zoo) error {
 }
 
 func main() {
-	simba := NewAnimal("lion", "Simba")
-	nala := NewAnimal("lion", "Nala")
-	pumba := NewAnimal("warthog", "Pumba")
-	timon := NewAnimal("meerkat", "Timon")
+	simba := NewAnimal("asdkjhasd", "Simba")
+	fmt.Println(simba.species)
+	nala := NewAnimal(Lion, "Nala")
+	pumba := NewAnimal(Warthog, "Pumba")
+	timon := NewAnimal(Meerkat, "Timon")
+	cage1 := NewCage("cage1")
+	cage2 := NewCage("cage2")
+	cage3 := NewCage("cage3")
+	cage4 := NewCage("cage4")
+	cage5 := NewCage("cage5")
 	zoopark := Zoo{
 		Animals: []*Animal{
 			simba,
@@ -89,39 +102,32 @@ func main() {
 			pumba,
 			timon,
 		},
-	}
-	cage1 := NewCage("cage1")
-	cage2 := NewCage("cage2")
-	cage3 := NewCage("cage3")
-	cage4 := NewCage("cage4")
-	cage5 := NewCage("cage5")
-	cages := AllCages{
-		Cages: []Cage{
-			*cage1,
-			*cage2,
-			*cage3,
-			*cage4,
-			*cage5,
+		Cages: []*Cage{
+			cage1,
+			cage2,
+			cage3,
+			cage4,
+			cage5,
 		},
 	}
 	zk := NewZookeeper()
-	err := zk.AddAnimalToCage(&cages, &zoopark)
-	// Якщо усі клітки зайняті то це ок, і доглядач зоопарку має сам вирішити це питання
+	err := zk.AddAnimalToCage(&zoopark)
+	// It's ok if all cages are busy, it's zookeeper's responsibility to figure out what to do next
 	if err != nil {
 		fmt.Println(err)
 	}
 	kiara := nala.Reproduce("lion", "Kiara")
 	zoopark.Animals = append(zoopark.Animals, kiara)
-	err = zk.AddAnimalToCage(&cages, &zoopark)
+	err = zk.AddAnimalToCage(&zoopark)
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, c := range cages.Cages {
+	for _, c := range zoopark.Cages {
 		c.Describe()
 	}
 	nala.Escape()
 	timon.Escape()
-	for _, c := range cages.Cages {
+	for _, c := range zoopark.Cages {
 		c.Describe()
 	}
 }
