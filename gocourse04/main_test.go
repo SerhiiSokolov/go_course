574abd7d6bc32f85f78cbcc9a0c1c814bd159977
Serhii Sokolov <serhii_sokolov@external.mckinsey.com>
2024-08-20 20:14:55 +0200

    feat: Homework4 Add tests

---
+++ gocourse04/main_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNewZoo(t *testing.T) {
+	zoo := NewZoo()
+	if zoo == nil {
+		t.Errorf("Expected new zoo to be created, got nil")
+	}
+}
+
+func TestAddArea(t *testing.T) {
+	zoo := NewZoo()
+	ungulatesArea := NewArea("A1", Ungulates)
+	zoo.AddArea("Ungulates", ungulatesArea)
+
+	if len(zoo.Areas) != 1 {
+		t.Errorf("Expected 1 area to be added, got %d", len(zoo.Areas))
+	}
+	if _, exists := zoo.Areas["Ungulates"]; !exists {
+		t.Errorf("Expected area 'Ungulates' to exist in the zoo, but it doesn't")
+	}
+}
+
+func TestNewArea(t *testing.T) {
+	area := NewArea("A1", Ungulates)
+	if area.Name != "A1" || area.Type != Ungulates {
+		t.Errorf("Expected area name to be A1 and type to be Ungulates, got %s and %s", area.Name, area.Type)
+	}
+}
+
+func TestNewSector(t *testing.T) {
+	const (
+		Brush Purpose = "Grooming Brush"
+	)
+	ungulatesArea := NewArea("A1", Ungulates)
+	brush := NewTool(Brush)
+	allTools := map[string]Tool{
+		"Brush": *brush,
+	}
+	ungulatesUtilityRoom := NewUtilityRoom(allTools)
+	sector := ungulatesArea.NewSector("deer", *ungulatesUtilityRoom)
+	if sector.Subtype != "deer" {
+		t.Errorf("Expected sector subtype to be 'deer', got '%s'", sector.Subtype)
+	}
+	if len(sector.Animals) != 0 {
+		t.Errorf("Expected new sector to have no animals, got %d", len(sector.Animals))
+	}
+	if sector.UtilityRoom.Tools == nil || sector.UtilityRoom.Tools["Brush"].Purpose != "Grooming Brush" {
+		t.Errorf("Expected new sector's utility room to have tool Brush with the Purpose \"Grooming Brush\", "+
+			"got %+v", sector.UtilityRoom.Tools["Brush"].Purpose)
+	}
+}
+
+func TestAddAnimal(t *testing.T) {
+	sector := &Sector{}
+	animal := NewAnimal(1, "Bambi", "deer")
+	sector.AddAnimal(animal)
+
+	if len(sector.Animals) != 1 {
+		t.Errorf("Expected sector to have 1 animal, got %d", len(sector.Animals))
+	}
+	if sector.Animals[0].Name != "Bambi" || sector.Animals[0].ID != 1 || sector.Animals[0].Subtype != "deer" {
+		t.Errorf("Expected sector's first animal to be 'Bambi' with ID 1 and subtype deer, got '%s' '%d' '%s' ",
+			sector.Animals[0].Name, sector.Animals[0].ID, sector.Animals[0].Subtype)
+	}
+}
+
+func TestFindAnimalByName(t *testing.T) {
+	zoo := NewZoo()
+	ungulatesArea := NewArea("A1", Ungulates)
+	zoo.AddArea("Ungulates", ungulatesArea)
+	sector := ungulatesArea.NewSector("deer", *NewUtilityRoom(nil))
+	animal := NewAnimal(1, "Bambi", "deer")
+	sector.AddAnimal(animal)
+
+	foundAnimal := zoo.FindAnimalByName("Bambi")
+	if foundAnimal == nil || foundAnimal.Name != "Bambi" {
+		t.Errorf("Expected to find animal 'Bambi', but it was not found")
+	}
+}
